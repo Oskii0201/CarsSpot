d7a8d94eb988e03be3eed19edccf6a0b40ee37f3
Oskar <oskar0201@onet.pl>
2024-09-04 14:26:02 +0200

    App.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { Header } from "./components/Header/Header.jsx";
-
+import element from "./assets/element.svg"
+import {Hero} from "./components/Hero/Hero.jsx";
+import {GalleryHeader} from "./components/GalleryHeader/GalleryHeader.jsx";
+import {Gallery} from "./components/Gallery/Gallery.jsx";
+import {Seo} from "./components/Seo/Seo.jsx";
+import {Footer} from "./components/Footer/Footer.jsx";
 export function App() {
   return (
-    /*<div className="bg-white relative">*/
     <div className="min-h-screen w-full bg-white">
-      <Header />
+        <img src={element} alt="Dekoracyjny element" className="absolute top-0 md:right-[10%] z-0"/>
+        <Header />
+        <main className="overflow-hidden">
+            <Hero/>
+            <GalleryHeader/>
+            <Gallery/>
+            <Seo/>
+        </main>
+        <Footer/>
     </div>
   );
 }

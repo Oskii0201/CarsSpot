7483c3a4ee2a5a2280bd4c80364dc459c9ee220f
Oskar <oskar0201@onet.pl>
2024-09-04 14:25:25 +0200

    hero refactor

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,8 +2,8 @@ import {Button} from "../Button/Button.jsx";
 
 export function Hero(){
     return(
-        <section id="hero" className="relative py-[120px] overflow-hidden h-[825px] ">
-            <div className="max-w-6xl mx-auto flex flex-col gap-12">
+        <section id="hero" className="relative py-10 overflow-hidden h-[560px] px-4 lg:px-0 lg:py-[120px] md:h-[730px] sm:h-[680px]">
+            <div className="relative max-w-6xl mx-auto flex flex-col gap-12 z-10">
                 <article className="sm:w-1/2 gap-8 text-darkgray flex flex-col">
                     <h1 className="font-bebas text-5xl font-normal leading-[84px] tracking-[-0.04em] text-left sm:text-7xl">
                         SPRZEDAJEMY SAMOCHODY Z EUROPY
@@ -18,7 +18,7 @@ export function Hero(){
                 </div>
             </div>
 
-            <div className="absolute bottom-[82px] right-0 w-3/4">
+            <div className="absolute bottom-[20px] right-0 w-[100%] sm:bottom-[40px] sm:w-[80%] md:bottom-[82px] md:w-3/4 lg:w-3/4">
                 <img src="/src/assets/shadow.png"
                      alt="Cień samochodów"
                      className="absolute bottom-0 right-0 h-auto object-cover opacity-70"

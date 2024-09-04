ab2cd32752ebe0482d4ee5f7a0167162f0e76e55
Oskar <oskar0201@onet.pl>
2024-09-04 15:05:29 +0200

    fix bug

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -3,10 +3,13 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Navigation, Pagination } from "swiper/modules";
+import alfa from "../../assets/alfa.jpeg"
+import audi from "../../assets/audi.jpeg"
+import maserati from "../../assets/maserati.jpeg"
+import porsche from "../../assets/porsche.jpeg"
+import street from "../../assets/street.jpeg"
 
-const images = import.meta.glob("/src/assets/carousel/*");
 export function Gallery() {
-    const imageList = Object.values(images);
 
     return (
         <section id="gallery" className="py-20 overflow-hidden outline-none">
@@ -35,15 +38,41 @@ export function Gallery() {
                 modules={[Pagination, Navigation]}
                 className="mySwiper pb-12"
             >
-                {imageList.map((image, index) => (
-                    <SwiperSlide key={index}>
-                        <img
-                            className="object-cover w-full h-96"
-                            src={image.name}
-                            alt={`Slide ${index + 1}`}
-                        />
-                    </SwiperSlide>
-                ))}
+                <SwiperSlide key={index}>
+                    <img
+                        className="object-cover w-full h-96"
+                        src={alfa}
+                        alt={`Slide 1`}
+                    />
+                </SwiperSlide>
+                <SwiperSlide key={index}>
+                    <img
+                        className="object-cover w-full h-96"
+                        src={audi}
+                        alt={`Slide 2`}
+                    />
+                </SwiperSlide>
+                <SwiperSlide key={index}>
+                    <img
+                        className="object-cover w-full h-96"
+                        src={maserati}
+                        alt={`Slide 3`}
+                    />
+                </SwiperSlide>
+                <SwiperSlide key={index}>
+                    <img
+                        className="object-cover w-full h-96"
+                        src={porsche}
+                        alt={`Slide 4`}
+                    />
+                </SwiperSlide>
+                <SwiperSlide key={index}>
+                    <img
+                        className="object-cover w-full h-96"
+                        src={street}
+                        alt={`Slide 5`}
+                    />
+                </SwiperSlide>
             </Swiper>
         </section>
     );
